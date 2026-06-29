3f16d49b1e42f9afa43c43a53856cfaf20d1e405
ssp <ssp2180481336@gmail.com>
2026-06-29 17:10:29 +0800

    清理临时文件

--- find-call.js
+++
@@ -1,5 +0,0 @@
-var c=require('fs').readFileSync('public/index.html','utf8');
-var needle = "liveSources[sym]||''";
-var i = c.indexOf(needle);
-console.log('found at', i);
-console.log(c.substring(i-80, i+80));
