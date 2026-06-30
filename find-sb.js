107c693c64e09232545e691a652ac064fecc71f2
ssp <ssp2180481336@gmail.com>
2026-06-30 16:55:53 +0800

    定投日历翻页:12月/页+◀▶箭头

--- find-sb.js
+++ find-sb.js
@@ -1,8 +1,5 @@
 var c=require('fs').readFileSync('public/index.html','utf8');
-var i=c.indexOf('setTimeout(function ptr');
-// find matching closing
-var depth=1, j=c.indexOf('{',i)+1;
+var i=c.indexOf('function renderLogHeatmap');
+var depth=1,j=c.indexOf('{',i)+1;
 while(depth>0&&j<c.length){if(c[j]==='{')depth++;if(c[j]==='}')depth--;j++}
-console.log('function body ends at', j);
-// now find the setTimeout closing paren
-console.log('chars after closing:', c.substring(j, j+5));
+console.log(c.substring(i,j));
