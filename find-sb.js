a521cd0c55690ab828a5244cc5bccaafcf9f97c9
ssp <ssp2180481336@gmail.com>
2026-06-30 16:32:18 +0800

    移动端下拉刷新:下拉>50px触发refreshPrices

--- find-sb.js
+++ find-sb.js
@@ -1,7 +1,3 @@
 var c=require('fs').readFileSync('public/index.html','utf8');
-var pos=0,re=/sidebar['"\)]\.|classList.*sidebar|sidebar.*classList/g;
-var m;
-while(m=re.exec(c)) {
-  console.log('--- match at', m.index, '---');
-  console.log(c.substring(Math.max(0,m.index-80), Math.min(m.index+120, c.length)).replace(/\n/g,' '));
-}
+var i=c.indexOf('tb.click();');
+console.log(c.substring(i,i+40));
