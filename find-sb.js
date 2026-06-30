3a61a01c3bf20d543b327938751fa0c1d0dea452
ssp <ssp2180481336@gmail.com>
2026-06-30 18:10:59 +0800

    修复SGOV盾牌:trig阈值40%→25%漏改

--- find-sb.js
+++ find-sb.js
@@ -1,5 +1,5 @@
 var c=require('fs').readFileSync('public/index.html','utf8');
-var i=c.indexOf('function renderLogHeatmap');
+var i=c.indexOf('function updateSgovPage');
 var depth=1,j=c.indexOf('{',i)+1;
 while(depth>0&&j<c.length){if(c[j]==='{')depth++;if(c[j]==='}')depth--;j++}
 console.log(c.substring(i,j));
