14a2378633df95ad0979de680003ee3ac3336878
ssp <ssp2180481336@gmail.com>
2026-07-09 22:45:09 +0800

    sw v9: force fresh load for pullHint fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var C="wealth-v8";
+var C="wealth-v9";
 self.addEventListener("install",function(e){self.skipWaiting()});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener("fetch",function(e){if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(function(cached){return fetch(e.request).then(function(res){if(res.ok){var r=res.clone();caches.open(C).then(function(c){c.put(e.request,r)})}return res}).catch(function(){return cached||new Response("Offline",{status:503})})}))});
