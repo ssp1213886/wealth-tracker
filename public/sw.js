54735e17bb193e81581e2c2d242be8ae26f5fda1
ssp1213886 <166212908+ssp1213886@users.noreply.github.com>
2026-09-24 22:17:27 +0800

    style: add breathing room between desktop sidebar sections, bump shell to v95

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-// Service Worker v94 - sidebar auto heights
-var CACHE = 'wealth-v94';
+// Service Worker v95 - sidebar breathing room
+var CACHE = 'wealth-v95';
 var PRECACHE = ['/', '/manifest.json', '/assets/main.css', '/assets/app.js'];
 
 function cacheResponse(request, response) {
